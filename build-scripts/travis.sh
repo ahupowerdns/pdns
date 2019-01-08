c1b0c137d04e68e08e9f9f173a5c0ffa2eb678bc
Peter van Dijk <peter.van.dijk@powerdns.com>
2019-01-08 12:05:13 +0100

    add lmdb-nodnssec testing target

--- build-scripts/travis.sh
+++ build-scripts/travis.sh
@@ -552,7 +552,7 @@ test_auth() {
 
   run "./timestamp ./start-test-stop 5300 tinydns"
 
-  run "./timestamp ./start-test-stop 5300 lmdb"
+  run "./timestamp ./start-test-stop 5300 lmdb-nodnssec"
 
   run "rm tests/ent-asterisk/fail.nsec"
 

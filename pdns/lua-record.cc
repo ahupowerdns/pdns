1bbbf75e08cda91a939e2c9aa25226375803f370
bert hubert <bert.hubert@powrdns.com>
2020-11-02 13:05:35 +0100

    implement localwho for Lua records in the authoritative server

--- pdns/lua-record.cc
+++ pdns/lua-record.cc
@@ -958,6 +958,7 @@ std::vector<shared_ptr<DNSRecordContent>> luaSynth(const std::string& code, cons
   lua.writeVariable("zone", zone);
   lua.writeVariable("zoneid", zoneid);
   lua.writeVariable("who", dnsp.getRemote());
+  lua.writeVariable("localwho", dnsp.getLocal());
   lua.writeVariable("dh", (dnsheader*)&dnsp.d);
   lua.writeVariable("dnssecOK", dnsp.d_dnssecOk);
   lua.writeVariable("tcp", dnsp.d_tcp);
